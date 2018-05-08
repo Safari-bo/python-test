9e38debdff4aa3d5689d4c516214071c9f7c389f
Safari-bo <867711956@qq.com>
2018-05-08 10:15:38 +0800

    Add network socket program test file.

---
+++ compass/apiclient/v1/bobo7.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from socket import *
+
+HOST = '120.79.173.182'
+PORT = 6002
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = raw_input('> ')
+    if not data:
+        break
+    tcpCliSock.send(data)
+    data = tcpCliSock.recv(BUFSIZ)
+    if not data:
+        break
+    print data
+
+tcpCliSock.close()
