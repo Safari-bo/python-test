07c8c3ca4cf3c6876a5605ab902135c7b46e3db4
Safari-bo <867711956@qq.com>
2018-06-14 10:53:40 +0800

    add function gather_intf_ip.

--- ip2csv.py
+++ ip2csv.py
@@ -19,7 +19,6 @@ if p1.returncode != 0:
     sys.exit(1)
 
 ip_dict = {}
-ip_list = []
 conf = ConfigParser.ConfigParser()
 conf.read('/var/lib/fitdep/playbooks/hosts_all')
 for entry in conf.options('optimize_nodes'):
@@ -27,32 +26,43 @@ for entry in conf.options('optimize_nodes'):
 print ip_dict.keys()
 
 
+def gather_intf_ip(ip, intf):
+    cmd = "ssh root@" + ip + " 'ip addr show {}'".format(intf)
+    out = commands.getoutput(cmd)
+    m = re.search('inet ((\d{1,3}\.){3}(\d{1,3}))', out)
+    if m:
+        return m.group(1)
+    else:
+        return "127.0.0.1"
+
+
 def execute_cmd(ip):
     ent = {"hostname": "host",
-             "pxe": "127.0.0.1",
-             "mgnt": "127.0.0.1",
-             "ipmi": "127.0.0.1",
-             "sn": "0123456789"}
+           "pxe": "127.0.0.1",
+           "mgnt": "127.0.0.1",
+           "service": "127.0.0.1",
+           "ceph_public": "127.0.0.1",
+           "ceph_cluster": "127.0.0.1",
+           "ipmi": "127.0.0.1",
+           "sn": "0123456789"}
     cmd = "ssh root@" + ip + " 'cat /etc/hostname'"
     ent['hostname'] = commands.getoutput(cmd).split('\r\n')[-1]
-    cmd = "ssh root@" + ip + " 'ip addr show brpxe'"
-    pxe = commands.getoutput(cmd)
-    if re.search('inet ((\d{1,3}\.){3}(\d{1,3}))', pxe) != None:
-        ent['pxe'] = re.search('inet ((\d{1,3}\.){3}(\d{1,3}))', pxe).group(1)
-    cmd = "ssh root@" + ip + " 'ip addr show mgnt'"
-    mgnt = commands.getoutput(cmd)
-    if re.search('inet ((\d{1,3}\.){3}(\d{1,3}))', mgnt) != None:
-        ent['mgnt'] = re.search('inet ((\d{1,3}\.){3}(\d{1,3}))', mgnt).group(1)
+
+    ent['pxe'] = gather_intf_ip(ip, 'brpxe')
+    ent['mgnt'] = gather_intf_ip(ip, 'mgnt')
+    ent['service'] = gather_intf_ip(ip, 'service')
+    ent['ceph_public'] = gather_intf_ip(ip, 'ceph-public')
+    ent['ceph_cluster'] = gather_intf_ip(ip, 'ceph-cluster')
+
     cmd = "ssh root@" + ip + " 'ipmitool lan print 1'"
     ipmi = commands.getoutput(cmd)
-    if re.search('IP Address.*: ((\d{1,3}\.){3}(\d{1,3}))', ipmi) != None:
+    if re.search('IP Address.*: ((\d{1,3}\.){3}(\d{1,3}))', ipmi):
         ent['ipmi'] = re.search('IP Address.*: ((\d{1,3}\.){3}(\d{1,3}))', ipmi).group(1)
     cmd = "ssh root@" + ip + " 'ipmitool fru'"
     sn = commands.getoutput(cmd)
-    if re.search('Product Serial.*: (\w.*)', sn) != None:
+    if re.search('Product Serial.*: (\w.*)', sn):
         ent['sn'] = re.search('Product Serial.*: (\w.*)', sn).group(1)
     if lock.acquire():
-        # ip_list.append(ent)
         ip_dict[ip] = ent
         lock.release()
 
@@ -64,12 +74,15 @@ for td in threads:
 for td in threads:
     td.join()
 
-# print ip_dict
+print ip_dict
 
 with open('./ip_dict.csv', 'w') as f:
-    header = ['Name', 'IPMI_IP', 'PXE_IP', 'Mgnt_IP', 'SN']
+    header = ['Name', 'IPMI_IP', 'PXE_IP', 'Mgnt_IP',
+              'Service', 'Ceph-public', 'Ceph-cluster', 'SN']
     file_writer = csv.writer(f)
     file_writer.writerow(header)
     for host in sorted(ip_dict.keys()):
-        entry = [ip_dict[host]['hostname'], ip_dict[host]['ipmi'], ip_dict[host]['pxe'], ip_dict[host]['mgnt'], ip_dict[host]['sn']]
+        entry = [ip_dict[host]['hostname'], ip_dict[host]['ipmi'], ip_dict[host]['pxe'],
+                 ip_dict[host]['mgnt'], ip_dict[host]['service'], ip_dict[host]['ceph_public'],
+                 ip_dict[host]['ceph_cluster'], ip_dict[host]['sn']]
         file_writer.writerow(entry)
